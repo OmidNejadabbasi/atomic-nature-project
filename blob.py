f738ca3a0b7a14729d6642e8b34a587ec22ec2a2
Omid.N <nejadabbasio@gmail.com>
2021-01-12 22:43:58 +0330

    Added comments to functions of Blob class

--- blob.py
+++ blob.py
@@ -2,6 +2,10 @@ import math
 
 
 class Blob:
+    """
+    A class representing a group of pixels connected together.
+    All functions have constant running complexity.
+    """
 
     def __init__(self):
         self.pix_list = []
@@ -11,6 +15,11 @@ class Blob:
         pass
 
     def add(self, x, y):
+        """
+        Adds the pixel with the specified coordinates x and y to the Blob
+        :param x: The x of the pixel
+        :param y: The y of the pixel
+        """
 
         self.avg_x = ((self.avg_x * self.mass()) + x) / (self.mass()+1)
         self.avg_y = ((self.avg_y * self.mass()) + y) / (self.mass()+1)
@@ -18,14 +27,22 @@ class Blob:
         self.pix_list += [[x, y]]
 
     def mass(self):
+        """
+        :return: Returns the number of pixels in this Blob
+        """
         return self.mass_val
 
     def str(self):
+        """
+        :return: Returns the string form of this Blob
+        """
         return '{} ({:.4f}, {:.4f})'.format(self.mass(), self.avg_x, self.avg_y)
 
     def distanceTo(self, c):
+        """
+        Computes the distance of center of this Blob with the other Blob c
+        """
         return math.sqrt((self.avg_x - c.avg_x) ** 2 + (self.avg_y - c.avg_y) ** 2)
 
     def __str__(self):
         return self.str()
-
