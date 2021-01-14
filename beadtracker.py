fc12df53fffac5fa120543b8f75f2fec002cb687
Omid.N <nejadabbasio@gmail.com>
2021-01-14 21:32:18 +0330

    first version of beadtracker.py

--- beadtracker.py
+++ beadtracker.py
@@ -1,5 +1,19 @@
 import sys
+import beadfinder
+import blob
+import picture
 
+
+def least_distance(b, blobs, delta):
+    least_dist = delta+1
+    for bl in blobs:
+        if b.distanceTo(bl) < least_dist:
+            least_dist = b.distanceTo(bl)
+
+    if least_dist == delta+1:
+        return None
+    else:
+        return least_dist
 
 def main():
     # getting inputs
@@ -7,6 +21,20 @@ def main():
     tau = float(sys.argv[2])
     delta = float(sys.argv[3])
     files = sys.argv[4:]
+
+    # -------- We will calculate the distance of each blob in sequential frames --------
+
+    # we store the first beadFinder in beadFinder1
+    beadFinder1 = beadfinder.BeadFinder(picture.Picture(files[0]), tau)
+
+    for i in range(1, len(files)):
+        bf = beadfinder.BeadFinder(picture.Picture(files[i]), tau)
+        for b in beadFinder1.getBeads(min_pixels):
+            distance = least_distance(b, bf.getBeads(min_pixels), delta)
+            if distance is not None:
+                print('{:.4f}'.format(distance))
+
+        beadFinder1 = bf
 
 
 
