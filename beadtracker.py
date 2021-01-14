9eae31cee90e932ea2d3c7ade63e3d085fe0c51a
Omid.N <nejadabbasio@gmail.com>
2021-01-14 12:12:07 +0330

    Getting inputs in beadtracker.py

---
+++ beadtracker.py
@@ -0,0 +1,14 @@
+import sys
+
+
+def main():
+    # getting inputs
+    min_pixels = int(sys.argv[1])
+    tau = float(sys.argv[2])
+    delta = float(sys.argv[3])
+    files = sys.argv[4:]
+
+
+
+if __name__ == '__main__':
+    main()
